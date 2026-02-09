0acb1cf6133eabb9d7fe0b5f261193f6b7b09c72
ThrinayaniReddy777555 <152727378+ThrinayaniReddy777555@users.noreply.github.com>
2026-02-09 22:02:34 +0530

    Add segregate method for sorting linked list
    Implement segregate method to sort linked list of 0's, 1's, and 2's.

---
+++ practice problems/sort_a_linkedlist_0's_and_1's.py
@@ -0,0 +1,36 @@
+'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+	
+class Solution:
+    def segregate(self, head):
+        #code here
+        if not head or not head.next:
+            return head
+        zeroD = Node(0)
+        oneD = Node(0)
+        twoD = Node(0)
+        zero = zeroD
+        one = oneD
+        two = twoD
+        curr = head
+        while curr:
+            if curr.data == 0:
+                zero.next = curr
+                zero = zero.next
+            elif curr.data == 1:
+                one.next = curr
+                one = one.next
+            else:
+                two.next = curr
+                two = two.next
+            curr = curr.next
+        zero.next = oneD.next if oneD.next else twoD.next
+        one.next = twoD.next
+        two.next = None 
+        head = zeroD.next
+        return head
+    
