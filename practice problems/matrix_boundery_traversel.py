6efd063120365fd8b063e4ad164e97392e398609
ThrinayaniReddy777555 <152727378+ThrinayaniReddy777555@users.noreply.github.com>
2026-02-10 15:55:00 +0530

    Refactor boundaryTraversal for matrix boundary traversal
    Refactor boundaryTraversal method for improved readability and efficiency.

--- practice problems/matrix_boundery_traversel.py
+++ practice problems/matrix_boundery_traversel.py
@@ -1,14 +1,30 @@
+
+        # code here 
+from typing import List
+
 class Solution:
-    def boundaryTraversal(self, mat):
-        arr = []
-        n = len(mat)        
-        m = len(mat[0])     
-        for j in range(n):
-            arr.append(mat[0][j])
-        for i in range(1, m):
-            arr.append(mat[i][m - 1])
-        for j in range(m - 2, -1, -1):
-            arr.append(mat[n - 1][j])
-        for i in range(n - 2, 0, -1):
-            arr.append(mat[i][0])
-        return arr
+    def boundaryTraversal(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix or not matrix[0]:
+            return []
+
+        n, m = len(matrix), len(matrix[0])
+        top, bottom = 0, n - 1
+        left, right = 0, m - 1
+
+        res = []
+
+        for j in range(left, right + 1):
+            res.append(matrix[top][j])
+
+        for i in range(top + 1, bottom + 1):
+            res.append(matrix[i][right])
+
+        if top != bottom:
+            for j in range(right - 1, left - 1, -1):
+                res.append(matrix[bottom][j])
+
+        if left != right:
+            for i in range(bottom - 1, top, -1):
+                res.append(matrix[i][left])
+
+        return res
