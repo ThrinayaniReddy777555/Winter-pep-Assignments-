5725afacc38e56be8df2c975076a2c51ed9d276a
ThrinayaniReddy777555 <152727378+ThrinayaniReddy777555@users.noreply.github.com>
2026-02-10 12:35:31 +0530

    Add minimum_size_subarray_sum.py solution
    Implement a solution to find the minimum size subarray sum that meets or exceeds a target value.

---
+++ practice problems/minimum_size_subarray_sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        n=len(nums)
+        left = 0
+        current_sum = 0
+        ans = float("inf")
+        for right in range(n):
+            current_sum += nums[right] 
+            print(current_sum)
+            while current_sum >= target:
+                ans = min(ans,right-left+1)
+                current_sum -= nums[left]
+                left += 1
+        return 0 if ans == float('inf')else ans
+
