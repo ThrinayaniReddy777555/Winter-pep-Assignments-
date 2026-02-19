d12bc68449d9404d85c41de9e7607c356928b454
ThrinayaniReddy777555 <152727378+ThrinayaniReddy777555@users.noreply.github.com>
2026-02-19 13:46:20 +0530

    add files

--- practice problems/all_nodes_distance_k_binarytree.py
+++ practice problems/all_nodes_distance_k_binarytree.py
@@ -1,32 +1,36 @@
-from collections import deque
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     def __init__(self, x):
+# #         self.val = x
+# #         self.left = None
+# #         self.right = None
+
+from collections import defaultdict, deque
 
 class Solution:
     def distanceK(self, root, target, k):
-        parent = {}
+        graph = defaultdict(list)
+        def build_graph(node, parent=None):
+            if node:
+                if parent:
+                    graph[node.val].append(parent.val)
+                    graph[parent.val].append(node.val)
+                build_graph(node.left, node)
+                build_graph(node.right, node)
         
-        def dfs(node, par):
-            if not node:
-                return
-            parent[node] = par
-            dfs(node.left, node)
-            dfs(node.right, node)
-        
-        dfs(root, None)
-        
-        queue = deque([(target, 0)])
-        visited = set([target])
+        build_graph(root)
+        queue = deque([(target.val, 0)])
+        visited = {target.val}
         result = []
         
         while queue:
-            node, dist = queue.popleft()
-            
-            if dist == k:
-                result.append(node.val)
-            
-            elif dist < k:
-                for nei in (node.left, node.right, parent[node]):
-                    if nei and nei not in visited:
-                        visited.add(nei)
-                        queue.append((nei, dist + 1))
+            node_val, distance = queue.popleft()
+            if distance == k:
+                result.append(node_val)
+            elif distance < k:
+                for neighbor in graph[node_val]:
+                    if neighbor not in visited:
+                        visited.add(neighbor)
+                        queue.append((neighbor, distance + 1))
         
         return result
