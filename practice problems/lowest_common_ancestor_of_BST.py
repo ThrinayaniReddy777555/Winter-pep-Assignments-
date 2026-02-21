87e3857c7b420754fcabe87bf709e408a3495745
ThrinayaniReddy777555 <152727378+ThrinayaniReddy777555@users.noreply.github.com>
2026-02-21 13:18:33 +0530

    add file

--- practice problems/lowest_common_ancestor_of_BST.py
+++ practice problems/lowest_common_ancestor_of_BST.py
@@ -15,16 +15,10 @@ class Solution:
         """
         node = root
         while node:
-            # If both p and q are greater than the current node's value,
-            # the LCA must be in the right subtree.
             if p.val > node.val and q.val > node.val:
                 node = node.right
-            # If both p and q are smaller than the current node's value,
-            # the LCA must be in the left subtree.
             elif p.val < node.val and q.val < node.val:
                 node = node.left
-            # Otherwise, the current node is the LCA, as p and q are on opposite sides
-            # or one of them is the current node itself.
             else:
                 return node
         return None # Should not be reached given problem constraints
